1d2d989ef31eecc0fa6d224468ea7633b544e392
Junior Javier <juniorllanos15@gmail.com>
2020-08-03 22:58:20 +0200

    lineas con forma de casa

---
+++ figura/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace figura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (figura game = new figura(800, 600, "LearnOpenTK"))
+            {
+                //Run takes a double, which is how many frames per second it should strive to reach.
+                //You can leave that out and it'll just update as fast as the hardware will allow it.
+                game.Run(60.0);
+            }
+        }
+    }
+}
